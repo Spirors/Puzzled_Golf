2216d1b39aaac3446d9106dc6250d402ab59b052
Siyuan Zou <Siyuanzou99@gmail.com>
2020-04-28 18:11:36 -0400

    line indicator added

--- public/src/objects/Ball.ts
+++ public/src/objects/Ball.ts
@@ -3,10 +3,15 @@ import { Level1 } from "../scenes/Level1";
 export class Ball extends Phaser.Physics.Arcade.Sprite {
     private max_power;
     private min_power;
-    private delta;
+    private ball_delta;
     private draggable;
+
     private max_indicatorLength;
     private min_indicatorLength;
+    private indicator_line : Phaser.Geom.Line;
+    private line_length;
+    private line_angle;
+    private graphics;
 
     constructor(config) {
         super(config.scene, config.x, config.y, 'ball');
@@ -20,16 +25,35 @@ export class Ball extends Phaser.Physics.Arcade.Sprite {
 
         this.max_power = 1000;
         this.min_power = 20;
-        this.delta = .985;
+        this.ball_delta = .985;
         this.draggable = true;
 
+        this.indicator_line = new Phaser.Geom.Line(
+            this.x,
+            this.y,
+            this.x,
+            this.y
+        );
         this.max_indicatorLength = 150;
         this.min_indicatorLength = 20;
+        this.line_length = 0;
+        this.line_angle = 0;
+        this.graphics = this.scene.add.graphics()
 
-        this.on('drag', function (pointer, gameObject) {
-            var angleToPointer = pointer.getAngle();
-            var distXToPointer = pointer.getDistanceX();
-            var distYToPointer = pointer.getDistanceY();
+        this.on('drag', function (pointer) {
+            if (this.draggable == true) {
+                var distXToPointer = pointer.getDistanceX();
+                var distYToPointer = pointer.getDistanceY();
+                this.line_angle = pointer.getAngle();
+                this.line_length = Math.sqrt(distXToPointer * distXToPointer + distYToPointer * distYToPointer);
+                if (this.line_length <= this.min_indicatorLength) {
+                    this.line_length = 0;
+                }
+                if (this.line_length > this.max_indicatorLength) {
+                    this.line_length = this.max_indicatorLength;
+                }
+                this.changeLine(this.line_length, this.line_angle);
+            }
         }, this);
 
         this.on('dragend', function (pointer) {
@@ -49,6 +73,7 @@ export class Ball extends Phaser.Physics.Arcade.Sprite {
                 if (power != 0) {
                     this.scene.events.emit("addScore");
                     this.draggable = false;
+                    this.changeLine(0, 0);
                     this.shootBall(power, angleToPointer);
                 }
             }
@@ -56,18 +81,30 @@ export class Ball extends Phaser.Physics.Arcade.Sprite {
     }
 
     update() {
+        this.updateBall();
+        this.updateLine();
+    }
+
+    updateBall() {
         // console.log(this.body.velocity.x, this.body.velocity.y);
         if (Math.abs(this.body.velocity.x) < 2 && Math.abs(this.body.velocity.x) < 2) {
             this.setVelocity(0, 0);
             this.draggable = true;
         } else {
-            this.setVelocity(this.delta * this.body.velocity.x, this.delta * this.body.velocity.y);
+            this.setVelocity(this.ball_delta * this.body.velocity.x, this.ball_delta * this.body.velocity.y);
         }
+    }
+    updateLine() {
+        this.graphics.clear();
+        this.graphics.lineStyle(2, 0xffffff, 1);
+        this.graphics.strokeLineShape(this.indicator_line);
+    }
+
+    changeLine(delta, angle) {
+        Phaser.Geom.Line.SetToAngle(this.indicator_line, this.x, this.y, angle + 3.14159, delta);
     }
 
     shootBall(power, angle) {
         this.setVelocity(power * -Math.cos(angle), power * -Math.sin(angle));
     }
-
-    // Todo: Line indicator
 }
