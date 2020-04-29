8f31b162fc9720d81de591f00f305dd1b8687de8
Kristy Tan <kristy.tan@stonybrook.edu>
2020-04-29 16:25:47 -0400

    added audio to splash screen

--- public/src/scenes/SplashScreen.ts
+++ public/src/scenes/SplashScreen.ts
@@ -2,6 +2,7 @@ export class SplashScreen extends Phaser.Scene{
     private cloud;
     private hill;
     private grass;
+    private music;
 
     constructor(){
         super("splashScreen");
@@ -15,13 +16,18 @@ export class SplashScreen extends Phaser.Scene{
         this.load.image("clouds", "../dist/assets/clouds.png");
         this.load.image("hills", "../dist/assets/hills.png");
         this.load.image("ground", "../dist/assets/ground.png");
+
+        this.load.audio("summer", "../dist/assets/audio/bensound-summer.mp3");
+        // this.load.spritesheet("golfer", "../dist/assets/male_folfer.png", {frameWidth: 128,
+        //     frameHeight: 128});
     }
     create(){
-        this.cameras.main.setBackgroundColor(0x9ce1e1);
+        this.cameras.main.setBackgroundColor(0x9ce1e1); //sky background color 
 
         var game_width = this.game.renderer.width;
         var game_height = this.game.renderer.height;
 
+        //add background layers 
         this.cloud = this.add.tileSprite(0,0, game_width, game_height, "clouds").setScale(1.37);
         this.hill = this.add.tileSprite(0,0, game_width, game_height, 'hills').setScale(1.37);
         this.grass = this.add.tileSprite(0,0, game_width, game_height, 'ground').setScale(1.37);
@@ -48,9 +54,22 @@ export class SplashScreen extends Phaser.Scene{
             this.scene.start('mainMenu');
         });
 
+        var music_config = {
+            mute: false,
+            volume: 1,
+            rate: 1,
+            detune: 0,
+            seek: 0,
+            loop: true,
+            delay: 0
+        }
+
+        this.music = this.sound.add("summer");
+        this.music.play(music_config);
     }
 
     update(){
+        //background layers scroll (clouds scroll slower than hill)
         this.cloud.tilePositionX -= 0.6;
         this.hill.tilePositionX -= 0.85;
         
