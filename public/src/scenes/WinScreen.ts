bfadadebd0b8d0971c083a2c9ef3e400ca075a2d
Billy <luybilly5@gmail.com>
2020-04-30 01:20:48 -0400

    added restart for level 2

--- public/src/scenes/WinScreen.ts
+++ public/src/scenes/WinScreen.ts
@@ -1,4 +1,5 @@
 import { Level1 } from "./Level1";
+import { Level2 } from "./Level2";
 
 export class winScreen extends Phaser.Scene{
     private parent;
@@ -7,6 +8,8 @@ export class winScreen extends Phaser.Scene{
     private score;
     private level;
     private starFrame;
+    private levelHighScore;
+
     constructor (handle, parent)
     {
         super(handle);
@@ -17,6 +20,7 @@ export class winScreen extends Phaser.Scene{
         this.score = data.score;
         this.menuHeight = 409;
         this.menuWidth = 626;
+        this.levelHighScore = localStorage.getItem("golfLevel"+ this.level +"HighScore");
 
         if (this.score <= 3) {
             this.starFrame = 3;
@@ -37,8 +41,10 @@ export class winScreen extends Phaser.Scene{
         // console.log(this.starFrame);
         var background = this.add.image(0,0,"win_bg").setOrigin(0);
         this.cameras.main.setViewport(this.game.renderer.width/2 - this.menuWidth/2, this.game.renderer.height/2 - this.menuHeight/2, this.menuWidth, this.menuHeight);
-        let star = this.add.sprite(this.menuWidth/2 , this.menuHeight/2 - 80, "stars", this.starFrame);
+        let star = this.add.sprite(this.menuWidth/2 , this.menuHeight/2 - 50, "stars", this.starFrame);
         star.setScale(1.5);
+        let scoreText = this.add.text(this.menuWidth/2 , this.menuHeight/2 + 50, 'Score - ' + this.score, { font: '20px Arial', fill: '#000000' });
+        let highScoreText = this.add.text(this.menuWidth/2 , this.menuHeight/2 + 70, 'Highscore - ' + this.levelHighScore, { font: '20px Arial', fill: '#000000' });
         var restart = this.add.image(this.menuWidth/2 - 120, this.menuHeight/2 + 100, "button", 5);
         var mainMenu = this.add.image(this.menuWidth/2 + 120, this.menuHeight/2 + 100, "button", 2);
         var nextLevel = this.add.image(this.menuWidth/2 , this.menuHeight/2 + 160, "button", 7);
@@ -66,6 +72,10 @@ export class winScreen extends Phaser.Scene{
                 this.scene.add('level1', Level1, true, {id: 1})
                 this.scene.stop();
             }
+            if(this.level == 2){
+                this.scene.add('level2', Level2, true, {id: 2})
+                this.scene.stop();
+            }
         });
 
         mainMenu.on('pointerdown', () => {
@@ -81,15 +91,6 @@ export class winScreen extends Phaser.Scene{
         nextLevel.on('pointerdown', () => {
             
         });
-
-        //go back to mainmenu
-        // console.log("highscore:", localStorage.getItem(this.localStorageName))
-        // ourGame.events.off('addScore');
-        // ourGame.events.off('setLevel');
-        // this.scene.remove("level" + this.level);
-        // this.scene.start("mainMenu");
-        // this.scene.remove("inGameMenu");
-        // this.scene.remove("hud");
     }
 
     refresh ()
