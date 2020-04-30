db55cc4b4652d1fe4ce054d09329da8392ab3d99
Billy <luybilly5@gmail.com>
2020-04-30 13:28:44 -0400

    help menu fixed

--- public/src/scenes/HelpMenu.ts
+++ public/src/scenes/HelpMenu.ts
@@ -1,17 +1,23 @@
 export class HelpMenu extends Phaser.Scene{
     private parent;
+    private menuWidth;
+    private menuHeight;
     constructor (handle, parent)
     {
         super(handle);
         this.parent = parent;
+    }
+    init(){
+        this.menuHeight = 502;
+        this.menuWidth = 662;
     }
     preload(){
         
     }
     create ()
     {
-        var background = this.add.image(0,0,"help_menu").setOrigin(0);
-        this.cameras.main.setViewport(this.parent.x, this.parent.y, 637, 361);
+        var background = this.add.image(0, 0,"help_menu").setOrigin(0);
+        this.cameras.main.setViewport(this.game.renderer.width/2 - this.menuWidth/2, this.game.renderer.height/2 - this.menuHeight/2, this.menuWidth, this.menuHeight);
         var exit = this.add.image(605, 10, "exit").setOrigin(0);
         exit.setDepth(1);
         exit.setInteractive();
